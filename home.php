f2e9329e418648c421126695f826ff94b4053994
Mikhail <314159265absurdus@gmail.com>
2016-08-24 19:00:10 +0300

    Добавил бэдж Бесплатно в схему работы

--- home.php
+++ home.php
@@ -70,7 +70,7 @@ get_header(); ?>
 			wp_reset_postdata();
 		 endwhile; ?>
 		 <section class="bgblock-workscheme clearfix">
-			 <h3 class="cta-home_title">
+			 <h3 class="cta-home_title" id="workscheme">
 			 	<?php _e('Схема работы'); ?>
 			 </h3>
 		<?php $showcta_home = new WP_Query('post_type=workscheme&posts_per_page=6');
